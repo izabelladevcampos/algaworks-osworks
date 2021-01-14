a827dfbfd6263c8e20056ac71a2b67f7df80db0e
Izabella Campos <izabellacampos2020@outlook.com>
2021-01-14 11:14:15 -0300

    Validações e tratamento de exceções

---
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/Problema.java
@@ -0,0 +1,69 @@
+package com.algaworks.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Problema {
+	
+	private Integer status;
+	private LocalDateTime dataHota;
+	private String titulo;
+	private List<Campos> campos;
+	
+	
+public static class Campos {
+	private String nome;
+	private String mensagem;
+	
+	
+	public Campos(String nome, String mensagem) {
+		super();
+		this.nome = nome;
+		this.mensagem = mensagem;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	
+}	
+	
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public LocalDateTime getDataHota() {
+		return dataHota;
+	}
+	public void setDataHota(LocalDateTime dataHota) {
+		this.dataHota = dataHota;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public List<Campos> getCampos() {
+		return campos;
+	}
+	
+	public void setCampos(List<Campos> campos) {
+		this.campos = campos;
+	}
+	
+	
+	
+}
